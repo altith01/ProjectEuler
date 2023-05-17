033ccd9a8a39c3097e520cad91c3db9efd369808
Tom Altier <tom4origami@gmail.com>
2023-05-16 21:39:37 -0500

    Solved 89

--- Unsolved/89.py
+++ Unsolved/89.py
@@ -1,1 +1,24 @@
 # Roman numerals
+
+numerals = open('89.txt', 'r').readlines()
+
+saved_total = 0
+for numeral in numerals:
+    saved = 0
+    numeral = numeral.strip()
+    length = len(numeral)
+    if 'IIII' in numeral:
+        saved += 2
+        if 'VIIII' in numeral:
+            saved += 1
+    if 'XXXX' in numeral:
+        saved += 2
+        if 'LXXXX' in numeral:
+            saved += 1
+    if 'CCCC' in numeral:
+        saved += 2
+        if 'DCCCC' in numeral:
+            saved += 1
+    saved_total += saved
+
+print(saved_total)
